6aced09662cbfca1bef96a38c0778363c31f7d7d
Shahid Ali <153486921+Shahid12ali@users.noreply.github.com>
2024-02-17 02:56:56 +0500

    update file

---
+++ Governor Assignmnt/main.js
@@ -0,0 +1,49 @@
+//Q2 ANS
+// let personName: string = "Shahid";
+// console.log(`Hello ${personName}, would you like to learn some Python today?`);
+//Q3 ANS
+// let personName: string = "Shahid Ali";
+// console.log("Lowercase:", personName.toLowerCase());
+// console.log("Uppercase:", personName.toUpperCase());
+// console.log("titlecase:", personName.replace(/\b\w/g,c=> c.toUpperCase()));
+//Q4 ANS
+// let quote: string="Life is like riding a bicycle. To keep your balance, you must keep moving.";
+// let author: string="Albert Einstein";
+// console.log(`${author} Once Said,"${quote}"`);
+//Q5 ANS
+// let quote: string = "Life is like riding a bicycle. To keep your balance, you must keep moving.";
+// let famous_person: string = "Albert Einstein";
+// let message: string = `"${quote}" - ${famous_person}`;
+// console.log(message);
+//Q6 ANS
+// var new_Name: string = "\t\n   Shahid Ali \n\t";
+// console.log(`Name with whitespace: "${new_Name}"`);
+// var my_Name: string = new_Name.trim();
+// console.log(`my name: "${my_Name}"`);
+//Q7 ANS
+// let Addition: number = 5 + 3;
+// console.log("Addition result:", Addition);
+// let Subtraction: number = 10 - 2;
+// console.log("Subtraction result:", Subtraction);
+// let Multiplication: number = 4 * 2;
+// console.log("Multiplication result:", Multiplication);
+// let Division: number = 16 / 2;
+// console.log("Division result:", Division);
+//Q9 ANS
+// let favoriteNumber:number = 8;
+// console.log(`My favorite Number is ${favoriteNumber}`);
+//Q10 ANS
+// author:[Shahid Ali];
+// date : [friday, february 16, 2024];
+//Q11 ANS
+//  let FriendsZ: string[] = ["Subhan", "Dibar", "Adnan", "Shahid"];
+//  for (let friendZ of `friends`) {
+//  console.log(`${friendZ}: We Are Not Friends Only, We Are Brothers`);
+//  }
+//Q13 ANS
+// var motors = ["Toyota", "Honda", "Bmw", "Rolls Royse", "Yaris"];
+// for (var _i = 0, motors_1 = motors; _i < motors_1.length; _i++) {
+//     var car = motors_1[_i];
+//     console.log("My All Times Favorite Cars ".concat(car, "."));
+// }
+//Q14 ANS
